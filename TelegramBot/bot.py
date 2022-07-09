61c11434a7dc3d6959f9e8c146a776df1bbf95ec
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-09 21:39:17 +0800

    inline handle test

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -1,10 +1,22 @@
 import telebot
+from telebot import types
 
 TOKEN = "5475633161:AAHJs4Y3Abac-BJb6GMj5ZR6-4mvZLI4bvM"
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Hello!! How may I help you?")
+
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
+
+bot.infinity_polling()
