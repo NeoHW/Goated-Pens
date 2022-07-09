45060fb3006b977f6a30f1494d27abeb82ffc0ac
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-09 23:14:35 +0800

    added choosing org

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -13,10 +13,17 @@ def gen_markup():
                                InlineKeyboardButton("Kids", callback_data="cb_kids"))
     return markup
 
+def choosing_org():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 3
+    markup.add(InlineKeyboardButton("MWS- Nursing Home", callback_data="cb_elderly"),
+                               InlineKeyboardButton("", callback_data="cb_kids"))
+    return markup
+
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "cb_elderly":
-        bot.send_message(call.message.chat.id, "Elderly! Which organisation ", reply_markup=gen_markup())
+        bot.send_message(call.message.chat.id, "Elderly! Which organisation do you want to assist?", reply_markup=gen_markup())
         bot.answer_callback_query(call.id, "Answer is Yes")
     elif call.data == "cb_kids":
         bot.answer_callback_query(call.id, "Answer is No")
