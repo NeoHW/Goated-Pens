8d7d39eee514c4bde9553ada445c8f34c1396959
zhanyang01 <ngzhanyang01@gmail.com>
2022-07-10 10:17:26 +0800

    copy paste a fk ton of shit

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -54,8 +54,152 @@ def choosing_kid_org():
                 InlineKeyboardButton("Make A Wish Foundation", callback_data="cb_mawf"))
     return markup
 
+def choosing_dates_eld_org_1():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_eld_org_2():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_eld_org_3():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_kid_org_1():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_kid_org_2():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_kid_org_3():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_sn_org_1():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_sn_org_2():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_sn_org_3():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_pet_org_1():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_pet_org_2():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
+def choosing_dates_pet_org_3():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+    markup.add( InlineKeyboardButton("Monday", callback_data="cb_mon"),
+                InlineKeyboardButton("Tuesday", callback_data="cb_tues"),
+                InlineKeyboardButton("Wednesday", callback_data="cb_wed"),
+                InlineKeyboardButton("Thursday", callback_data="cb_thurs"),
+                InlineKeyboardButton("Friday", callback_data="cb_fri"),
+                InlineKeyboardButton("Saturday", callback_data="cb_sat"),
+                InlineKeyboardButton("Sunday", callback_data="cb_sun")),
+    return markup
+
 @bot.callback_query_handler(func=None)
-def callback_query(call):
+def callback_query_choosing_org(call):
     if call.data == "cb_elderly":
         bot.send_message(call.message.chat.id, "Elderly! Which organisation do you want to assist?", reply_markup=choosing_eld_org())
         bot.answer_callback_query(call.id, "Thank you for assisting our elderly")
@@ -69,7 +213,44 @@ def callback_query(call):
         bot.send_message(call.message.chat.id, "Pets! Which organisation do you want to assist?", reply_markup=choosing_pet_org())
         bot.answer_callback_query(call.id, "Thank you for assisting our animals") 
 
-    
+@bot.callback_query_handler(func=None)
+def callback_query_choosing_dates(call):
+    if call.data == "cb_mwsnh":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_eld_org_1())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_bnh":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_eld_org_2())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_phnh":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_eld_org_3())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_scs":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_kid_org_1())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_cr":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_kid_org_2())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_mawf":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_kid_org_3())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_minds":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_sn_org_1())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_hwa":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_sn_org_2())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_dsa":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_sn_org_3())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_asd":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_pet_org_1())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_htr":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_pet_org_2())
+        bot.answer_callback_query(call.id, "Thank you so much!")
+    elif call.data == "cb_ltv":
+        bot.send_message(call.message.chat.id, "Thanks you for helping us, may i ask when you are free to assist us?", reply_markup=choosing_dates_pet_org_3())
+        bot.answer_callback_query(call.id, "Thank you so much!")
 def main():   
     bot.polling()
 
